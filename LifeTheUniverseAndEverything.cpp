ec717b444a7b839218768366a8aa1e06048d9ba1
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2018-11-30 19:46:19 +0100

    Create LifeTheUniverseAndEverything.cpp

---
+++ LifeTheUniverseAndEverything.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std; // consider removing this line in serious projects
+ 
+int main() {
+	auto func = [] () {
+		int intNum = 0;
+		cin >> intNum;
+		while (intNum != 42) {
+			cout << intNum << "\n";
+			cin >> intNum;
+		}
+	};
+	func();
+	cout << 42 << "\n";
+	fflush(stdout);
+ 
+	return 0;
+}
